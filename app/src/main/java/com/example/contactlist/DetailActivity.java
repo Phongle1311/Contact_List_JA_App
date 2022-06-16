a6ac669e35af054780363f6011b4372509d84427
Phongle1311 <hoaiphong13.11.2002@gmail.com>
2022-06-16 09:44:43 +0700

    fix intent from main activity to detail activity

--- app/src/main/java/com/example/contactlist/DetailActivity.java
+++ app/src/main/java/com/example/contactlist/DetailActivity.java
@@ -5,6 +5,8 @@ import static java.util.ResourceBundle.getBundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.contactlist.module.Contact;
@@ -20,8 +22,31 @@ public class DetailActivity extends AppCompatActivity {
         if (bundle == null)
             return;
 
+
+
         Contact contact = (Contact) bundle.get("object_contact");
-        TextView textView = findViewById(R.id.tv_name_detail);
-        textView.setText(contact.getName());
+        ImageView imgAvtDetail = findViewById(R.id.img_avt_detail);
+        TextView tvNameDetail = findViewById(R.id.tv_name_detail);
+        ImageButton btnImportantDetail = findViewById(R.id.btn_important_detail);
+        TextView tvMobilePhoneDetail = findViewById(R.id.tv_mobile_phone_detail);
+        TextView tvWorkPhoneDetail = findViewById(R.id.tv_work_phone_detail);
+        TextView tvPersonalMailDetail = findViewById(R.id.tv_personal_mail_detail);
+        TextView tvWorkMailDetail = findViewById(R.id.tv_work_mail_detail);
+
+
+        imgAvtDetail.setImageResource(contact.getResourceID());
+        tvNameDetail.setText(contact.getName());
+        int whiteStarID = getResources().getIdentifier("white_circle_star" ,
+                "drawable", getPackageName());
+        int pinkStarID = getResources().getIdentifier("pink_circle_star" ,
+                "drawable", getPackageName());
+        if (contact.getImportant() == true)
+            btnImportantDetail.setImageResource(pinkStarID);
+        else
+            btnImportantDetail.setImageResource(whiteStarID);
+        tvMobilePhoneDetail.setText(contact.getMobilePhoneNumber());
+        tvWorkPhoneDetail.setText(contact.getWorkPhoneNumber());
+        tvPersonalMailDetail.setText(contact.getPersonMail());
+        tvWorkMailDetail.setText(contact.getWorkMail());
     }
 }
--- app/src/main/java/com/example/contactlist/MainActivity.java
+++ app/src/main/java/com/example/contactlist/MainActivity.java
@@ -41,31 +41,31 @@ public class MainActivity extends AppCompatActivity {
     private List<Contact> getListContacts() {
         List<Contact> list = new ArrayList<>();
 
-        Contact contact1 = new Contact("Lê Hoài Phong", R.drawable.ic_save_name,
+        Contact contact1 = new Contact("Lê Hoài Phong", R.drawable.hangouts_icon,
                 "0387671963", "0387671963",
                 "hoaiphong13.11.2002@gmail.com", "20120545@student.hcmus.edu.vn", true);
         contact1.setType(2);
         list.add(contact1);
 
-        Contact contact2 = new Contact("Contact 2", R.drawable.ic_save_name,
+        Contact contact2 = new Contact("Contact 2", R.drawable.hangouts_icon,
                 "0387671963", "0387671963",
                 "hoaiphong13.11.2002@gmail.com", "20120545@student.hcmus.edu.vn", true);
         contact2.setType(1);
         list.add(contact2);
 
-        Contact contact3 = new Contact("Contact 3", R.drawable.ic_save_name,
+        Contact contact3 = new Contact("Contact 3", R.drawable.hangouts_icon,
                 "0387671963", "0387671963",
                 "hoaiphong13.11.2002@gmail.com", "20120545@student.hcmus.edu.vn", true);
         contact3.setType(1);
         list.add(contact3);
 
-        Contact contact4 = new Contact("Contact 4", R.drawable.ic_save_name,
+        Contact contact4 = new Contact("Contact 4", R.drawable.hangouts_icon,
                 "0387671963", "0387671963",
                 "hoaiphong13.11.2002@gmail.com", "20120545@student.hcmus.edu.vn", true);
         contact4.setType(2);
         list.add(contact4);
 
-        Contact contact5 = new Contact("Contact 5", R.drawable.ic_save_name,
+        Contact contact5 = new Contact("Contact 5", R.drawable.hangouts_icon,
                 "0387671963", "0387671963",
                 "hoaiphong13.11.2002@gmail.com", "20120545@student.hcmus.edu.vn", true);
         contact5.setType(1);
